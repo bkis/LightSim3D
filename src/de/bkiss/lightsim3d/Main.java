53091fd24c4d8adbf219cb4edd9c1d9e1fcac77e
kritzelbit <boerge.kiss@gmail.com>
2015-02-17 18:11:11 +0100

    added test scene

--- src/de/bkiss/lightsim3d/Main.java
+++ src/de/bkiss/lightsim3d/Main.java
@@ -1,16 +1,19 @@
 package de.bkiss.lightsim3d;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Spatial;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
 
 /**
- * test
- * @author normenhansen
+ * Main class for LightSim3D
+ * @author Börge Kiss
  */
 public class Main extends SimpleApplication {
 
@@ -21,14 +24,33 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        //table
+        Spatial table = assetManager.loadModel("Models/Table/table.obj");
+        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        mat.setTexture("DiffuseMap", assetManager.loadTexture("Models/Table/diffuse.tga"));
+        mat.setTexture("NormalMap", assetManager.loadTexture("Models/Table/normal.tga"));
+        mat.setTexture("SpecularMap", assetManager.loadTexture("Models/Table/spec.tga"));
+        table.setMaterial(mat);
+        table.scale(0.05f);
+        table.move(0, -2, 0);
+        table.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
+        rootNode.attachChild(table);
+        
+        //ambient light
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        rootNode.addLight(ambient); 
+        
+        //sunlight
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun); 
+        
+        //shadow renderer
+        DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, 2048, 4);
+        dlsr.setLight(sun);
+        viewPort.addProcessor(dlsr); 
     }
 
     @Override
