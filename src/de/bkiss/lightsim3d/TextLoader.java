a5450af793ddad3eb4bd6c7073f2f7da95b260f5
bkis <boerge.kiss@gmail.com>
2015-02-20 14:55:57 +0100

    hud text, TextLoader

--- src/de/bkiss/lightsim3d/TextLoader.java
+++ src/de/bkiss/lightsim3d/TextLoader.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.bkiss.lightsim3d;
 
 import com.jme3.asset.AssetInfo;
@@ -23,7 +19,7 @@ public class TextLoader implements AssetLoader {
                 sb.append(scan.nextLine()).append("\n");
             }
         } finally {
-        scan.close();
+            scan.close();
         }
         return sb.toString();
     }
